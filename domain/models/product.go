517ade100cc8c5c05d4f16bf46726e50559ddd15
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-13 16:43:14 -0300

    refactor product model

--- domain/models/product.go
+++ domain/models/product.go
@@ -2,6 +2,6 @@ package models
 
 type Product struct {
 	ID    int     `gorm:"primaryKey" json:"id"`
-	Price float64 `gorm:"column:price" json:"price" `
+	Price float64 `gorm:"column:price" json:"price"`
 	Name  string  `gorm:"column:name" json:"name"`
 }
