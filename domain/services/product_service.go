aac6cf69d01719d7406bb1aea9d0ebec593e1cf6
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-13 17:07:15 -0300

    add all services

--- domain/services/product_service.go
+++ domain/services/product_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/peidrao/eulabs-api/domain/models"
 	"github.com/peidrao/eulabs-api/domain/repositories"
 	"github.com/peidrao/eulabs-api/utils"
@@ -13,6 +15,9 @@ type productService struct {
 
 type ProductService interface {
 	Create(product models.Product) utils.Response
+	Delete(productId int) utils.Response
+	Update(productId int, product models.Product) utils.Response
+	GetById(productId int) utils.ProductResponse
 }
 
 func NewProductService(db *gorm.DB) ProductService {
@@ -22,11 +27,39 @@ func NewProductService(db *gorm.DB) ProductService {
 func (service *productService) Create(product models.Product) utils.Response {
 	var response utils.Response
 	if err := service.productRepo.Create(product); err != nil {
-		response.Status = 400
-		response.Messages = "Failed to create a new product"
-	} else {
 		response.Status = 200
 		response.Messages = "Success to create a new product"
 	}
+	return response
+}
+
+func (service *productService) Delete(productId int) utils.Response {
+	var response utils.Response
+	if err := service.productRepo.Delete(productId); err != nil {
+		response.Status = 400
+		response.Messages = fmt.Sprint("Failed to delete product: ", productId)
+	} else {
+		response.Status = 200
+		response.Messages = "Success to delete product"
+	}
+	return response
+}
+
+func (service *productService) Update(productId int, product models.Product) utils.Response {
+	var response utils.Response
+	if err := service.productRepo.Update(productId, product); err != nil {
+		response.Status = 400
+		response.Messages = fmt.Sprint("Failed to update product: ", productId)
+	} else {
+		response.Status = 200
+		response.Messages = "Success to update product"
+	}
+	return response
+}
+
+func (service *productService) GetById(productId int) utils.ProductResponse {
+	var response utils.ProductResponse
+	data, _ := service.productRepo.Get(productId)
+	response.Product = data
 	return response
 }
