4d3521022af9faaef3eb7b0d13b0f813744aa706
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-13 16:42:53 -0300

    add all methods in repository

--- domain/repositories/product_repository.go
+++ domain/repositories/product_repository.go
@@ -11,6 +11,9 @@ type dbProduct struct {
 
 type ProductRepository interface {
 	Create(product models.Product) error
+	Delete(productId int) error
+	Update(productId int, product models.Product) error
+	Get(productId int) (models.Product, error)
 }
 
 func NewProductRepository(Conn *gorm.DB) ProductRepository {
@@ -19,4 +22,18 @@ func NewProductRepository(Conn *gorm.DB) ProductRepository {
 
 func (db *dbProduct) Create(product models.Product) error {
 	return db.Conn.Create(&product).Error
+}
+
+func (db *dbProduct) Delete(productId int) error {
+	return db.Conn.Delete(&models.Product{ID: productId}).Error
+}
+
+func (db *dbProduct) Update(productId int, product models.Product) error {
+	return db.Conn.Where("id", productId).Updates(product).Error
+}
+
+func (db *dbProduct) Get(productId int) (models.Product, error) {
+	var data models.Product
+	result := db.Conn.Where("id", productId).First(&data)
+	return data, result.Error
 }
