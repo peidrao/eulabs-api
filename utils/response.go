f42893db9957ae80117586c023a9c09f0340f32b
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-13 17:06:59 -0300

    add product response

--- utils/response.go
+++ utils/response.go
@@ -1,7 +1,13 @@
 package utils
 
+import "github.com/peidrao/eulabs-api/domain/models"
+
 type Response struct {
 	Status   int         `json:"status"`
 	Messages string      `json:"messages"`
 	Data     interface{} `json:"data"`
+}
+
+type ProductResponse struct {
+	Product models.Product `json:"product"`
 }
